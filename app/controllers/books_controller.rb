745ae699a58bdfb04f520a03007d402e79be161c
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-07-29 12:11:38 +0000

    [Add]詳細ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,41 @@
 class BooksController < ApplicationController
+  # 投稿画面用
   def new
     @book = Book.new
   end
-
+  
+  # 投稿機能
+  def create
+    @book = Book.new(book_params)
+    # 投稿者id=ログインユーザid
+    @book.user_id = current_user.id
+    @book.save
+    # @bookのid=パスの引数
+    redirect_to book_path(@book.id)
+  end
+  
+# 本一覧取得
   def index
+    @books = Book.all
   end
 
+# 本詳細表示
+# 表示中のid=レコード（params）の引数
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+  
+  
+  private
+  
+  def book_params
+    # 投稿データのストロングパラメータ
+    # Bookモデルに関係したtitle,bodyカラムのみに限定
+    params.require(:book).permit(:title, :body)
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: "about"
   
   # resourceを使って自動生成
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:new, :create, :index, :show]
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
