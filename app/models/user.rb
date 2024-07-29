bf85f282b8bdb356eaad885735b3ebacdbb5c0da
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-07-29 11:07:57 +0000

    [Add]Bookコントローラ・投稿ページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
          has_one_attached :image
+         
+        # アソシエーション
+         has_many :books, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,32 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   # 名前付きルーティング
   get 'homes/about' => 'homes#about', as: "about"
+  
+  # resourceを使って自動生成
+  resources :books, only: [:new, :index, :show]
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
+#           user_session POST   /users/sign_in(.:format)     devise/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)     devise/sessions#destroy
+#       new_user_password GET    /users/password/new(.:format)     devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format)    devise/passwords#edit
+#           user_password PATCH  /users/password(.:format)          devise/passwords#update
+#                         PUT    /users/password(.:format)   devise/passwords#update
+#                         POST   /users/password(.:format)     devise/passwords#create
+# cancel_user_registration GET    /users/cancel(.:format)    devise/registrations#cancel
+#   new_user_registration GET    /users/sign_up(.:format)      devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)    devise/registrations#edit
+#       user_registration PATCH  /users(.:format)  devise/registrations#update
+#                         PUT    /users(.:format) devise/registrations#update
+#                         DELETE /users(.:format)  devise/registrations#destroy
+#                         POST   /users(.:format)   devise/registrations#create
+#                     root GET    /        homes#top
+#                   about GET    /homes/about(.:format)    homes#about
+#                   books GET    /books(.:format)      books#index
+#                 new_book GET    /books/new(.:format)         books#new
+#                     book GET    /books/:id(.:format)      books#show
